93253bc0fc5e914dc62f5077b2b95f645c7dff1b
beijing.wangyan <beijing.wangyan@fang.com>
2018-04-13 10:12:50 +0800

    测试

--- src/main/java/com/sso/dao/AuthLoginDao.java
+++ src/main/java/com/sso/dao/AuthLoginDao.java
@@ -5,5 +5,5 @@ import com.sso.domain.*;
 public interface AuthLoginDao {
 	public abstract void insertAuthLogin(AuthLogin username);
 
-	public abstract AuthLogin queryAuthLoginByName(String username);
+	public abstract AuthLogin queryAuthLoginByName(String username);  
 }
